de8c6a4b8b0f04d562d2a53caff20635db3b431b
K.SRI RAM <119724502+22MH1A42G5@users.noreply.github.com>
2022-12-15 22:09:28 +0530

    #include<stdio.h> int main() {     int a,b,c,i,j,ans;     scanf("%d%d",&a,&b);     c=a+b;     if(c>10)     {         printf("%d",21-c);     }     else     {         printf("-1");     } }

---
+++ Blackjack.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,i,j,ans;
+    scanf("%d%d",&a,&b);
+    c=a+b;
+    if(c>10)
+    {
+        printf("%d",21-c);
+    }
+    else
+    {
+        printf("-1");
+    }
+}
