53933a8020c9a32658a31d12b0d63b5ab7786536
K.SRI RAM <119724502+22MH1A42G5@users.noreply.github.com>
2022-12-16 10:24:48 +0530

    #include<stdio.h> int main() {     int n,i,sum=0,firstn=0;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         sum=sum+i*i;         firstn=firstn+i;     }
        printf("%d",(firstn*firstn)-sum);
    }

---
+++ Difference_of_Sums.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,sum=0,firstn=0;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        sum=sum+i*i;
+        firstn=firstn+i;
+    }
+    
+    printf("%d",(firstn*firstn)-sum);
+}
