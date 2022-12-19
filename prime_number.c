a5ca057e7b7a91d5ba53f03bcd9d0c5bf2b3ecc4
K.SRI RAM <119724502+22MH1A42G5@users.noreply.github.com>
2022-12-19 20:38:13 +0530

    #include<stdio.h> int main() {     int a,b=0,i,j;     scanf("%d",&a);     for(i=1;i<=a;i++)     {         if(a%i==0)         {             b++;         }     }     if(b==2)     {         printf("prime");     }     else     {         printf("not a prime");     } }

---
+++ prime_number.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int a,b=0,i,j;
+    scanf("%d",&a);
+    for(i=1;i<=a;i++)
+    {
+        if(a%i==0)
+        {
+            b++;
+        }
+    }
+    if(b==2)
+    {
+        printf("prime");
+    }
+    else
+    {
+        printf("not a prime");
+    }
+}
