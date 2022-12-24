821ff2a2949ba0475473fe263cb58d654d8bfe17
K.SRI RAM <119724502+22MH1A42G5@users.noreply.github.com>
2022-12-24 22:03:22 +0530

    #include<stdio.h> int main() {     int a,b,max;     scanf("%d%d",&a,&b);     if(a>b)     {         max=a;     }     else     {         max=b;     }     for(max;;max++)     {         if(max%a==0 && max%b==0)         {             printf("%d",max);             break;         }     } }

---
+++ LCM.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,max;
+    scanf("%d%d",&a,&b);
+    if(a>b)
+    {
+        max=a;
+    }
+    else
+    {
+        max=b;
+    }
+    for(max;;max++)
+    {
+        if(max%a==0 && max%b==0)
+        {
+            printf("%d",max);
+            break;
+        }
+    }
+}
