a666be01953fafebbf6f198de2adac9005f68e44
K.SRI RAM <119724502+22MH1A42G5@users.noreply.github.com>
2023-01-01 15:08:33 +0530

    #include<stdio.h> int main() {     int l,n,i,w,h;     scanf("%d%d",&l,&n);     for(i=1;i<=n;i++)     {         scanf("%d%d",&w,&h);         if(w<l || h<l)         {             printf("UPLOAD ANOTHER ");         }         else if(w>=l || h>=l)         {             if(w==h)             {                 printf("ACCEPTED ");             }             else             {                 printf("CROP IT ");             }         }
        }
    }

---
+++ Roy_and_Profile_Picture.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int l,n,i,w,h;
+    scanf("%d%d",&l,&n);
+    for(i=1;i<=n;i++)
+    {
+        scanf("%d%d",&w,&h);
+        if(w<l || h<l)
+        {
+            printf("UPLOAD ANOTHER
+");
+        }
+        else if(w>=l || h>=l)
+        {
+            if(w==h)
+            {
+                printf("ACCEPTED
+");
+            }
+            else
+            {
+                printf("CROP IT
+");
+            }
+        }
+        
+        
+    }
+}
