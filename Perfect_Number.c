4560efdcea98b941bb0860fd5b04d409a0ce2ae5
K.SRI RAM <119724502+22MH1A42G5@users.noreply.github.com>
2023-01-04 23:05:05 +0530

    #include<stdio.h> int main() {     int a,b=0,i;     scanf("%d",&a);     for(i=1;i<=a-1;i++)     {         if(a%i==0)         {             b=b+i;         }     }     if(a==b)     {         printf("True");     }     else     {         printf("False");     } }

---
+++ Perfect_Number.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int a,b=0,i;
+    scanf("%d",&a);
+    for(i=1;i<=a-1;i++)
+    {
+        if(a%i==0)
+        {
+            b=b+i;
+        }
+    }
+    if(a==b)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+}
