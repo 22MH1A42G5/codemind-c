0306b41bdb610fc79beacd59419a73bff5ec9410
K.SRI RAM <119724502+22MH1A42G5@users.noreply.github.com>
2023-06-02 18:55:46 +0530

    #include<stdio.h> int maxi(int a[],int n) {     int m=0,i;     for(i=0;i<n;i++)     {         if(a[i]>m){             m=a[i];         }     }     return m; } int main(){     int n,i,max,t;     scanf("%d",&n);     int a[n];     for(i=0;i<n;i++)     scanf("%d",&a[i]);     scanf("%d",&t);     max=maxi(a,n);     for(i=0;i<n;i++)     {         if(a[i]+t>=max)         printf("True ");         else         printf("False ");     } }

--- Greatest_Number_of_Candies.c
+++ Greatest_Number_of_Candies.c
@@ -4,16 +4,14 @@ int maxi(int a[],int n)
     int m=0,i;
     for(i=0;i<n;i++)
     {
-        if(a[i]>m)
-        {
+        if(a[i]>m){
             m=a[i];
         }
     }
     return m;
 }
-int main()
-{
-    int n,max,i,t;
+int main(){
+    int n,i,max,t;
     scanf("%d",&n);
     int a[n];
     for(i=0;i<n;i++)
