a3d1714f1fa4cb8653cdd63f7756b30c4bfcf5be
K.SRI RAM <119724502+22MH1A42G5@users.noreply.github.com>
2023-06-03 10:30:44 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     for(int i=0;i<n;i++)     {         for(int j=0;j<n;j++)         {             if(i==j || i+j==n-1)             {                 printf("x");             }             else             printf("0");         }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -3,11 +3,16 @@ int main()
 {
     int n;
     scanf("%d",&n);
-    for(int i=1;i<=n;i++)
+    for(int i=0;i<n;i++)
     {
-        for(int j=1;j<=n;j++)
+        for(int j=0;j<n;j++)
         {
-            printf("%c ",i+64);
+            if(i==j || i+j==n-1)
+            {
+                printf("x");
+            }
+            else
+            printf("0");
         }
         printf("
 ");
